21ad5e1bac463c695748c7d17963e0c8837b73ca
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-05 22:00:35 +0700

    add feature for logout

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,7 +1,10 @@
+import 'package:chat_app/common/styles.dart';
+import 'package:chat_app/view_model/chat_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -10,8 +13,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var viewModel = Provider.of<ChatViewModele>(context, listen: false);
     return Scaffold(
-      body: Column(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            onPressed: () async {
+              await viewModel.logout();
+            },
+            icon: const Icon(
+              Icons.logout,
+              color: Styles.primaryColor,
+            )),
+      ),
+      body: const Column(
         children: [],
       ),
     );
