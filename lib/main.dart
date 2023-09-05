2e7937d80f46bda968f9f5d16f65a1509c2eda90
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-05 14:35:24 +0700

    add widget binding in main for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:chat_app/view/login_screen.dart';
+import 'package:chat_app/view/auth/login_screen.dart';
+import 'package:chat_app/view/auth/register_screen.dart';
 import 'package:chat_app/view_model/chat_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => ChatViewModele())],
     child: const MyApp(),
