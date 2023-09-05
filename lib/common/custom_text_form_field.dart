0901260f4ed9ebdbaf7db75cb6e40dd3f7f8722e
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-05 21:59:44 +0700

    add final in variable

---
+++ lib/common/custom_text_form_field.dart
@@ -0,0 +1,57 @@
+import 'package:chat_app/common/styles.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomTextFormField extends StatefulWidget {
+  final String hintTExt;
+  final Icon prefixIcon;
+  final TextEditingController controller;
+  final FormFieldValidator validator;
+  final bool obsecure;
+  final Widget? suffix;
+  const CustomTextFormField(
+      {Key? key,
+      required this.hintTExt,
+      required this.controller,
+      required this.validator,
+      required this.obsecure,
+      required this.prefixIcon,
+      this.suffix})
+      : super(key: key);
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      style: Styles.reguler,
+      obscureText: widget.obsecure,
+      validator: widget.validator,
+      decoration: InputDecoration(
+        hintStyle: Styles.reguler,
+        contentPadding: EdgeInsets.zero,
+        hintText: widget.hintTExt,
+        prefixIcon: widget.prefixIcon,
+        filled: true,
+        fillColor: Styles.whiteColor,
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(width: 1, color: Styles.whiteColor)),
+        focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(width: 1, color: Colors.redAccent)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(width: 1, color: Styles.whiteColor)),
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(width: 1, color: Colors.redAccent)),
+        suffixIcon: widget.suffix,
+      ),
+    );
+  }
+}
