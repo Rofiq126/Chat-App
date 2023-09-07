476d0685b5ccf24fa75ed5ffa9c86a2eccef015b
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-07 22:51:56 +0700

    edit user model

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,14 +1,35 @@
 class UserModel {
+  String? docId;
+  String? username;
+  String? lastMessage;
   String email;
+  DateTime? createdAt;
   String password;
 
-  UserModel({required this.email, required this.password});
+  UserModel(
+      {required this.email,
+      required this.password,
+      this.docId,
+      this.username,
+      this.createdAt,
+      this.lastMessage});
 
   UserModel.fromJson(Map<String, dynamic> json)
       : email = json['email'],
+        docId = json['docId'],
+        createdAt = json['createdAt'],
+        username = json['username'],
+        lastMessage = json['lastMessage'],
         password = json['password'];
 
   userModelToJson() {
-    return {'email': email, 'password': password};
+    return {
+      'email': email,
+      'docId': docId,
+      'createdAt': createdAt,
+      'password': password,
+      'username': username,
+      'lastMessage': lastMessage
+    };
   }
 }
