e264b08c2cf58b6a216910167b90f77710e537fe
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-07 22:53:40 +0700

    adding widget for build the chat in chat screen

---
+++ lib/view/chat_screen/component/build_chat.dart
@@ -0,0 +1,36 @@
+import 'package:chat_app/common/styles.dart';
+import 'package:chat_app/view_model/chat_view_model.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+Widget buildChat(
+    {required AsyncSnapshot snapshot, required ChatViewModele viewModel}) {
+  return ListView.separated(
+    separatorBuilder: (context, index) {
+      return const SizedBox(
+        height: 5,
+      );
+    },
+    itemCount: snapshot.data!.docs.length,
+    itemBuilder: (context, index) {
+      var data = snapshot.data!.docs[index];
+      viewModel.lastMessage = data['message'];
+
+      var time = data['timestamp'];
+      DateTime datetime = time.toDate();
+      viewModel.chatTime = DateFormat.Hm().format(datetime);
+      bool isSender =
+          data['senderId'] == viewModel.firebaseAuth.currentUser!.uid
+              ? true
+              : false;
+      return BubbleSpecialThree(
+        text: data['message'],
+        isSender: isSender,
+        color: isSender ? Styles.secondryColor : Styles.whiteColor,
+        tail: true,
+        textStyle: isSender ? Styles.regulerWhite : Styles.regulerSecondry,
+      );
+    },
+  );
+}
